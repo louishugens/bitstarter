40f44442ebf5328fdad56a19b2f02f7da295573c
Louis Hugens <louishugens@gmail.com>
2013-07-11 17:05:38 +0000

    Updated again 2 web.js

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(32);
-buffer.write(fs.readFileSync(index.html));
+buffer.write(fs.readFileSync('index.html'));
  
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8", 0, 28));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
